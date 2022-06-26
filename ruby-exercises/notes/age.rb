00a2447502835d9f031fabcffe288c06c4892937
Skraevern <86933999+Skraevern@users.noreply.github.com>
2022-06-26 16:22:39 +0200

    Added notes to ruby

---
+++ ruby-exercises/notes/age.rb
@@ -0,0 +1,8 @@
+puts "Whats your age?"
+age = gets.chomp.to_i
+puts "In 10 years you wil be:"
+puts age + 10  
+puts "In 20 years you wil be:"
+puts age + 20
+puts "In 30 years you wil be:"
+puts age + 30
---
+++ ruby-exercises/notes/name.rb
@@ -0,0 +1,13 @@
+puts "Whats your name?"
+name = gets.chomp
+puts "Hello " + name
+
+10.times do
+    puts name
+end
+
+puts "Whats your first name?"
+first_name = gets.chomp
+puts "Watsh your last name?"
+last_name = gets.chomp
+puts "Hello there " + first_name + " " + last_name
