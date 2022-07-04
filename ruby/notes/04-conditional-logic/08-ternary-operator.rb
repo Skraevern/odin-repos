192b4d02451d46e95fedc5086ceda938298ef8b6
Skraevern <86933999+Skraevern@users.noreply.github.com>
2022-07-04 20:47:41 +0200

    Added ruby conditional statements

---
+++ ruby/notes/04-conditional-logic/08-ternary-operator.rb
@@ -0,0 +1,12 @@
+age = 19
+response = age < 18 ? "You still have your entire life ahead of you." : "You're all grown up."
+puts response #=> "You're all grown up."
+
+age = 19
+if age < 18
+  response = "You still have your entire life ahead of you."
+else
+  response = "You're all grown up."
+end
+
+puts response #=> "You're all grown up."
