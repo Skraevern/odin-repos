9ff76dbe3cfb8896d7a47ef451934774f9ec3d82
Skraevern <86933999+Skraevern@users.noreply.github.com>
2022-07-17 11:26:58 +0200

    Finished odin 09 debugging notes and exercises

---
+++ ruby/notes/09-debugging/ruby-guides/02-pry.rb
@@ -0,0 +1,7 @@
+<!-- All you have to do is drop binding.pry where you would like to install a pry breakpoint.
+
+You will also need to require pry into your project (require 'pry').
+
+If you just want to do it temporarily then you can call your ruby script like this:
+
+ruby -rpry app.rb -->
