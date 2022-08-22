9a18d4ffbdb9733ce3f4b0cc58507f0ef4ed1a4e
Skraevern <kristian.skreosen@icloud.com>
2022-08-22 08:30:41 +0200

    Added data-bases-and-sql odin notes

---
+++ 02-fullstack-ruby-on-rail/03-databases/02-databases-and-sql/01-odin/02-learning-outcomes.sql
@@ -0,0 +1,45 @@
+Learning Outcomes
+Look through these now and then use them to test yourself after doing the assignment:
+
+Important Concepts:
+What is the Primary Key?
+What are Foreign Keys?
+What is a Schema?
+
+Statements:
+SELECT
+CREATE TABLE
+DROP TABLE
+CREATE INDEX
+DROP INDEX
+UPDATE
+DELETE
+INSERT INTO
+CREATE DATABASE
+DROP DATABASE
+COMMIT (concept)
+ROLLBACK (concept)
+
+Clauses:
+DISTINCT
+WHERE
+IN
+AND
+OR
+BETWEEN
+LIKE
+ORDER BY
+COUNT
+
+Functions:
+GROUP BY
+HAVING
+AVG
+COUNT
+MIN
+MAX
+SUM
+
+Other Stuff:
+What are Indexes good for?
+What’s the difference between WHERE and HAVING?
