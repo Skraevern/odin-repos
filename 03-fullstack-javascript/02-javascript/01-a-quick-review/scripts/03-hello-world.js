d2d05a7b6f0fe766c94b09ba8cf2777b09bd0ab7
Skraevern <86933999+Skraevern@users.noreply.github.com>
2022-11-19 13:58:14 +0100

    Added files

--- 03-fullstack-javascript/02-javascript/01-a-quick-review/scripts/03-hello-world.js
+++ 03-fullstack-javascript/02-javascript/01-a-quick-review/scripts/03-hello-world.js
@@ -1,4 +1,2 @@
 const myHeading = document.querySelector("h1");
 myHeading.textContent = "Hello World!";
-
-console.log("ttest");
---
+++ 03-fullstack-javascript/02-javascript/02-organizing-js/02-objects-and-objext-constructors/01-intro.js
@@ -0,0 +1,13 @@
+// // Introduction
+
+// In our JavaScript fundamentals course, you should have learned the basics of using objects to store and retrieve data. Let’s start with a little refresher.
+
+// There are multiple ways to define objects but in most cases, it is best to use the object literal syntax as follows:
+
+const myObject = {
+    property: "Value!",
+    otherProperty: 77,
+    "obnoxious property": function () {
+        // do stuff!
+    },
+};
